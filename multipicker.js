68b37c7dd8bd773751ff088190efc6c7ba11b8a7
Stepan Vardanyan <styopdev@hotmail.com>
2016-01-04 02:55:50 +0400

    Make it possible to create more than one picker instance, bug fixes, optimizations.

--- multipicker.js
+++ multipicker.js
@@ -1,214 +1,231 @@
-var multiPicker = {
-	selector: null,
-	lastElem: null,
-	options: {},
+var MultiPicker = function () {
+	this.options  	 = {
+		valueSource: "index",
+		prePopulate: null
+	};
+	this.lastElem 	 = "";
+	this.input    	 = null;
+	this.selector 	 = null;
 
-	setEvendHandlers: function() {
-		$(multiPicker.selector + " li").click(function(){
-			multiPicker.select.call(this, false);
+	this.setEvendHandlers = function () {
+		var picker = this;
+		$(this.selector + " li").click(function () {
+			picker.select.call(this, picker, false);
 		});
 
-		$(multiPicker.selector + " li").mousemove(function(e) {
-			if ((e.which || e.button) && multiPicker.lastElem != e.target) {
-				multiPicker.hover(e);
-				multiPicker.lastElem = e.target;
+		$(this.selector + " li").mousemove(function (e) {
+			if ((e.which || e.button) && picker.lastElem != e.target) {
+				picker.hover(e);
+				picker.lastElem = e.target;
 			}
 		});
 
-		$(multiPicker.selector + " li").mouseup(multiPicker.finishHover);
-	},
+		$(this.selector + " li").mouseup(this.finishHover);
+	};
 
-	hover: function(e) {
+	this.hover = function (e) {
 		var element = $(e.target);
-		multiPicker.select.call(element);
-	},
+		this.select.call(element, this, false);
+	};
 
-	finishHover: function(e) {
-		multiPicker.lastElem = null;
-	},
+	this.finishHover = function (e) {
+		this.lastElem = null;
+	};
 
-	select: function(isPrepopulated) {
+	//	arguments: element as this, picker, isPrepopulated flag which is true only on init
+	this.select = function (picker, isPrepopulated) {
 		var selectedVal;
 
-		if (multiPicker.options.valueSource == "index") {
+		if (picker.options.valueSource == "index") {
 			selectedVal = $(this).index();
-		} else if (multiPicker.options.valueSource.substring(0, 5) == "data-") {
-			selectedVal = $(this).attr(multiPicker.options.valueSource);
+		} else if (picker.options.valueSource.substring(0, 5) == "data-") {
+			selectedVal = $(this).attr(picker.options.valueSource);
 		} else {
 			selectedVal = $(this).text();
 		}
 
-		if (multiPicker.options.isSingle) {
-			multiPicker.clear();
+		if (picker.options.isSingle) {
+			picker.clear();
 
-			$(this).siblings("." + multiPicker.options.activeClass).removeClass();
-			$(this).addClass(multiPicker.options.activeClass);
+			$(this).siblings("." + picker.options.activeClass).removeClass();
+			$(this).addClass(picker.options.activeClass);
 
-			multiPicker.addValue(selectedVal);
+			picker.addValue(selectedVal);
 			return;
 		}
-		// unselect case
-		if ($(this).hasClass(multiPicker.options.activeClass)) {
+		if ($(this).hasClass(picker.options.activeClass)) {
+			// unselect case
 			$(this).removeClass();
 
-			multiPicker.removeValue(selectedVal);
-			// select case
-			multiPicker.updateClasses($(this), multiPicker.options.activeClass);
-			if (multiPicker.options.onUnselect && typeof multiPicker.options.onUnselect === "function" && !isPrepopulated) {
-				multiPicker.options.onUnselect(this, selectedVal);
+			picker.removeValue(selectedVal);
+			MultiPicker.updateClasses($(this), picker.options.activeClass);
+			if (picker.options.onUnselect && typeof picker.options.onUnselect === "function" && !isPrepopulated) {
+				picker.options.onUnselect(this, selectedVal);
 			}
 		} else {
-			$(this).addClass(multiPicker.options.activeClass);
+			// select case
+			$(this).addClass(picker.options.activeClass);
 
-			multiPicker.addValue(selectedVal);
+			picker.addValue(selectedVal);
 			
-			multiPicker.updateClasses($(this), multiPicker.options.activeClass);
-			if (multiPicker.options.onSelect && typeof multiPicker.options.onSelect === "function" && !isPrepopulated) {
-				multiPicker.options.onSelect(this, selectedVal);
+			MultiPicker.updateClasses($(this), picker.options.activeClass);
+			if (picker.options.onSelect && typeof picker.options.onSelect === "function" && !isPrepopulated) {
+				picker.options.onSelect(this, selectedVal);
 			}
 		}
-	},
+	};
 
-	addValue: function(val) {
-		var currValue = $("[name=" + multiPicker.options.inputName + "]").val();
+	this.addValue = function (val) {
+		var currValue = this.input.val();
 		if (currValue) {
 			currValue += ",";
 		}
 		currValue += val;
 
-		$("[name=" + multiPicker.options.inputName + "]").val(currValue);
-	},
+		this.input.val(currValue);
+	};
 
-	removeValue: function(val) {
-		var currValue = $("[name=" + multiPicker.options.inputName + "]").val();
+	this.removeValue = function (val) {
+		var currValue = this.input.val();
 		currValue = currValue.replace("," + val, "").replace(val + ",", "").replace(val, "");
 
-		$("[name=" + multiPicker.options.inputName + "]").val(currValue);
-	},
+		this.input.val(currValue);
+	};
 
-	clear: function() {
-		$("[name=" + multiPicker.options.inputName + "]").val("");	
-	},
+	this.clear = function () {
+		this.input.val("");
+	};
 
-	updateClasses: function(item, className) {
-		if ($(item).hasClass(className)) {
-			if ($(item).next().hasClass(className) && $(item).prev().hasClass(className)) {
-				if ($(item).next().next().hasClass(className)) {
-					$(item).next().removeClass();
-					$(item).next().addClass(className + " center-side");
-				} else {
-					$(item).next().removeClass();
-					$(item).next().addClass(className + " right-side");
-				}
-				if ($(item).prev().prev().hasClass(className)) {
-					$(item).prev().removeClass();
-					$(item).prev().addClass(className + " center-side");
-				} else {
-					$(item).prev().removeClass();
-					$(item).prev().addClass(className + " left-side");
-				}
-				$(item).removeClass();
-				$(item).addClass("active center-side");
-			} else if ($(item).next().hasClass(className) && !$(item).prev().hasClass(className)) {
-				if ($(item).next().next().hasClass(className)) {
-					$(item).next().removeClass();
-					$(item).next().addClass(className + " center-side");
-				} else {
-					$(item).next().removeClass();
-					$(item).next().addClass(className + " right-side");
-				}
-				$(item).removeClass();
-				$(item).addClass("active left-side");
-			} else if (!$(item).next().hasClass(className) && $(item).prev().hasClass(className)) {
-				if ($(item).prev().prev().hasClass(className)) {
-					$(item).prev().removeClass();
-					$(item).prev().addClass(className + " center-side");
-				} else {
-					$(item).prev().removeClass();
-					$(item).prev().addClass(className + " left-side");
-				}
-				$(item).removeClass();
-				$(item).addClass(className + " right-side");
+	this.prePopulate = function () {
+		if (MultiPicker.isArray(this.options.prePopulate) && this.options.prePopulate.length) {
+			for (var key in this.options.prePopulate) {
+				var searched = this.options.prePopulate[key];
+				var element = this.getPrepopulateSelector(searched);
+
+				if ($(element).index() < 0)
+					console.warn("Multipicker: prepopulated element doesn`t found `%s`", searched);
+				else
+					this.select.call(index, this, true);
 			}
 		} else {
-			if ($(item).next().hasClass("right-side")) {
+			var element = this.getPrepopulateSelector(this.options.prePopulate);
+			if ($(element).index() < 0)
+				console.warn("Multipicker: prepopulated element doesn`t found`%s`", this.options.prePopulate);
+			else
+				this.select.call(element, this, true);
+		}
+	};
+
+	this.getPrepopulateSelector = function (searched) {
+		if (this.options.valueSource == "index" || !this.options.valueSource) {
+			return $(this.selector + " li:eq(" + searched + ")");
+		} else if (this.options.valueSource.substring(0, 5) == "data-") {
+			return $(this.selector + " li[" + this.options.valueSource + "='" + searched + "']");
+		} else if (this.options.valueSource == "text") {
+			return $(this.selector + " li:contains('" + searched + "')");
+		}
+	};
+};
+
+MultiPicker.isArray = function (obj) {
+	if (Object.prototype.toString.call(obj) === '[object Array]') {
+		return true;
+	}
+}
+
+MultiPicker.updateClasses = function (item, className) {
+	if ($(item).hasClass(className)) {
+		if ($(item).next().hasClass(className) && $(item).prev().hasClass(className)) {
+			if ($(item).next().next().hasClass(className)) {
 				$(item).next().removeClass();
-				$(item).next().addClass(className);
-			}
-			if ($(item).prev().hasClass("left-side")) {
-				$(item).prev().removeClass();
-				$(item).prev().addClass(className);
-			}
-			if ($(item).prev().hasClass("center-side")) {
-				$(item).prev().removeClass();
-				$(item).prev().addClass(className + " right-side");
-			}
-			if ($(item).next().hasClass("center-side")) {
+				$(item).next().addClass(className + " center-side");
+			} else {
 				$(item).next().removeClass();
-				$(item).next().addClass(className + " left-side");
+				$(item).next().addClass(className + " right-side");
 			}
-		}
-	},
-
-	prePopulate: function() {
-		if (multiPicker.isArray(multiPicker.options.prePopulate) && multiPicker.options.prePopulate.length) {;
-			for (var key in multiPicker.options.prePopulate) {
-				var searched = multiPicker.options.prePopulate[key];
-				multiPicker.select.call(multiPicker.getPrepopulateSelector(searched), true);
+			if ($(item).prev().prev().hasClass(className)) {
+				$(item).prev().removeClass();
+				$(item).prev().addClass(className + " center-side");
+			} else {
+				$(item).prev().removeClass();
+				$(item).prev().addClass(className + " left-side");
 			}
-		} else {
-			multiPicker.select.call(multiPicker.getPrepopulateSelector(multiPicker.options.prePopulate), true);
-		}
-	},
-
-	getPrepopulateSelector: function(searched) {
-		if (multiPicker.options.valueSource == "index" || !multiPicker.options.valueSource) {
-			return $(multiPicker.selector + " li:eq(" + searched + ")");
-		} else if (multiPicker.options.valueSource.substring(0, 5) == "data-") {
-			return $(multiPicker.selector + " li[" + multiPicker.options.valueSource + "='" + searched + "']");
-		} else if (multiPicker.options.valueSource == "text") {
-			if (multiPicker.isArray(multiPicker.options.prePopulate)) {
-				return $(multiPicker.selector + " li:contains('" + searched + "')");
+			$(item).removeClass();
+			$(item).addClass("active center-side");
+		} else if ($(item).next().hasClass(className) && !$(item).prev().hasClass(className)) {
+			if ($(item).next().next().hasClass(className)) {
+				$(item).next().removeClass();
+				$(item).next().addClass(className + " center-side");
+			} else {
+				$(item).next().removeClass();
+				$(item).next().addClass(className + " right-side");
 			}
+			$(item).removeClass();
+			$(item).addClass("active left-side");
+		} else if (!$(item).next().hasClass(className) && $(item).prev().hasClass(className)) {
+			if ($(item).prev().prev().hasClass(className)) {
+				$(item).prev().removeClass();
+				$(item).prev().addClass(className + " center-side");
+			} else {
+				$(item).prev().removeClass();
+				$(item).prev().addClass(className + " left-side");
+			}
+			$(item).removeClass();
+			$(item).addClass(className + " right-side");
 		}
-	},
-
-	isArray: function(obj) {
-		if (Object.prototype.toString.call(obj) === '[object Array]') {
-			return true;
+	} else {
+		if ($(item).next().hasClass("right-side")) {
+			$(item).next().removeClass();
+			$(item).next().addClass(className);
+		}
+		if ($(item).prev().hasClass("left-side")) {
+			$(item).prev().removeClass();
+			$(item).prev().addClass(className);
+		}
+		if ($(item).prev().hasClass("center-side")) {
+			$(item).prev().removeClass();
+			$(item).prev().addClass(className + " right-side");
+		}
+		if ($(item).next().hasClass("center-side")) {
+			$(item).next().removeClass();
+			$(item).next().addClass(className + " left-side");
 		}
 	}
 };
 
 jQuery.fn.extend({
-	multiPicker: function(opt) {
-		multiPicker.options = opt;
-		multiPicker.selector = "#" + this.attr("id");
+	multiPicker: function (opt) {
+		// init picker instance
+		var picker = new MultiPicker();
+		picker.options 	= Object.assign(picker.options, opt);
+		picker.selector = "#" + this.attr("id");
+		picker.input 	= $("[name=" + picker.options.inputName + "]");
 
-		if (!$("[name='" + multiPicker.options.inputName + "']").length) {
-			throw "Input with name `" + multiPicker.options.inputName + "` does`nt exist";
+		if (!picker.input.length) {
+			throw "Input with name `" + picker.options.inputName + "` does`nt exist";
 		}
 
-		if (multiPicker.options.prePopulate && multiPicker.options.prePopulate.length > 1 && multiPicker.options.isSingle) {
+		if (picker.options.prePopulate && MultiPicker.isArray(picker.options.prePopulate) && picker.options.prePopulate.length > 1 && picker.options.isSingle) {
 			throw "Can not prePopulate more then 1 item, with `isSingle` true option";
 		}
 
-		if (multiPicker.options.valueSource && !(multiPicker.options.valueSource != "index" || multiPicker.options.valueSource != "text" || multiPicker.options.valueSource.substring(0, 5) == "data-")) {
+		if (picker.options.valueSource && !(picker.options.valueSource != "index" || picker.options.valueSource != "text" || picker.options.valueSource.substring(0, 5) == "data-")) {
 			throw "Invalid value source";
 		}
 
-		if (!multiPicker.options.activeClass) {
-			multiPicker.options.activeClass = "active";
+		if (!picker.options.activeClass) {
+			picker.options.activeClass = "active";
 		}
 
-		if (multiPicker.options.prePopulate) {
-			multiPicker.prePopulate();
+		if (picker.options.prePopulate) {
+			picker.prePopulate();
 		}
 
-		multiPicker.setEvendHandlers();
+		$(picker.selector).attr("ondragstart", 'return false');
+		picker.setEvendHandlers();
 
-		if (multiPicker.options.onInit && typeof multiPicker.options.onInit === "function") {
-			multiPicker.options.onInit();
+		if (picker.options.onInit && typeof picker.options.onInit === "function") {
+			picker.options.onInit();
 		}
 	}
 });
